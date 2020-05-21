9a4474d5c58007136e2c1f9cce433d06e8b8c821
Drumania <martinbrumana@gmail.com>
2020-05-21 18:44:20 -0300

    actua

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,40 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+//import axios from "axios";
+import { postings } from "../api/category_50___1.js";
+import ThumbProduct from "./ThumbProduct";
 
 const GridProduct = () => {
-  const [category, setCategory] = useState([]);
+  const [productsHome, setProductsHome] = useState([]);
 
   useEffect(() => {
+    let products = postings[0].products;
+
+    //direction
+    console.log(products);
+    setProductsHome(products);
+
+    /*
     const getProducts = async () => {
       const url = "../api/category_50___1.json";
       const productsByCategory = await axios.get(url);
 
-      console.log(productsByCategory);
+      setCategory(productsByCategory);
     };
 
     getProducts();
+    */
   }, []);
 
-  return <div>
-
-            <div class="single-product-wrapper">
-              <!-- Product Image -->
-              <div class="product-img">
-                <img src="img/product-img/product-1.jpg" alt="">
-                <!-- Hover Thumb -->
-                <img class="hover-img" src="img/product-img/product-2.jpg" alt="">
-                <!-- Favourite -->
-                <div class="product-favourite">
-                  <a href="#" class="favme fa fa-heart"></a>
-                </div>
-              </div>
-              <!-- Product Description -->
-              <div class="product-description">
-                <span>topshop</span>
-                <a href="single-product-details.html">
-                  <h6>Knot Front Mini Dress</h6>
-                </a>
-                <p class="product-price">$80.00</p>
-
-                <!-- Hover Content -->
-                <div class="hover-content">
-                  <!-- Add to Cart -->
-                  <div class="add-to-cart-btn">
-                    <a href="#" class="btn essence-btn">Add to Cart</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
-  </div>;
+  return (
+    <div className="container">
+      <h1 className="my-3">Ofertas!</h1>
+      <div className="wrap-thumbs-products">
+        {productsHome.map((product) => (
+          <ThumbProduct key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default GridProduct;
---
+++ src/components/ThumbProduct.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const thumbProduct = ({ product }) => {
+  return (
+    <div className="single-product-wrapper p-3" id={product.id}>
+      <div className="product-img">
+        <img src={product.photo_thumb} alt={product.name} />
+
+        <img className="hover-img" src={product.photo} alt={product.name} />
+
+        <div className="product-favourite">
+          <a href="#!" className="favme fa fa-heart">
+            <i className="far fa-heart"></i>
+          </a>
+        </div>
+      </div>
+
+      <div className="product-description">
+        <span>{product.category}</span>
+        <a href="#!">
+          <h6>{product.name}</h6>
+        </a>
+        <p className="product-price">{`$ ${product.price}`}</p>
+
+        <div className="hover-content">
+          <div className="add-to-cart-btn">
+            <a href="#!" className="btn essence-btn">
+              Agregar al carrito
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default thumbProduct;
