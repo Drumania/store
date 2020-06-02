87d79bf82a6e8e890d4118fa01f898e71aee0bef
Drumania <martinbrumana@gmail.com>
2020-06-02 16:47:39 -0300

    filters hardcodeados

--- src/components/Category.js
+++ src/components/Category.js
@@ -13,7 +13,80 @@ const Category = () => {
         </div>
       </div>
       <div className="container-white row rounded">
-        <div className="col-12 col-lg-3 border-right">sidebar</div>
+        <div className="col-12 col-lg-3 border-right">
+          <ul className="mb-4">
+            <li>36 resultados</li>
+          </ul>
+
+          <ul className="mb-4">
+            <li>
+              <h4>Tiendas </h4>
+            </li>
+            <li>
+              <a href="#!">Makkax</a>
+            </li>
+            <li>
+              <a href="#!">Harse</a>
+            </li>
+            <li>
+              <a href="#!">Intex</a>
+            </li>
+          </ul>
+
+          <ul className="mb-4">
+            <li>
+              <h4>Marcas</h4>
+            </li>
+            <li>
+              <a href="#!">Samsung</a>
+            </li>
+            <li>
+              <a href="#!">Philips</a>
+            </li>
+            <li>
+              <a href="#!">Sanyo</a>
+            </li>
+          </ul>
+
+          <ul className="mb-4">
+            <li>
+              <h4>Descuento</h4>
+            </li>
+            <li>
+              <a href="#!">40 %</a>
+            </li>
+            <li>
+              <a href="#!">27 %</a>
+            </li>
+            <li>
+              <a href="#!">10 %</a>
+            </li>
+          </ul>
+
+          <ul className="mb-4">
+            <li>
+              <h4>Precio</h4>
+            </li>
+            <li>
+              <a href="#!">Hasta $ 20.000</a>
+            </li>
+            <li>
+              <a href="#!">$20.000 a $60.000</a>
+            </li>
+            <li>
+              <a href="#!">Más de $60.000</a>
+            </li>
+          </ul>
+
+          <ul className="mb-4">
+            <li>
+              <h4>Productos Seleccionados</h4>
+            </li>
+            <li>
+              <a href="#!">Tele 1</a>
+            </li>
+          </ul>
+        </div>
         <div className="col-9">
           <ListProducts productsHome={categoria[0].products} />
         </div>
